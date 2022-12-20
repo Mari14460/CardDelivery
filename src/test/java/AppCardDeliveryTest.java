1130666ed2031329846090739ba6d0448735e6ff
Marina Zinchenko <amanda79116@gmail.com>
2022-12-20 17:02:17 -0600

    removed selector for form, added method for date, improved condition for verification

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,12 +1,11 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -18,23 +17,26 @@ public class AppCardDeliveryTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[id='root']");
-        form.$("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar c = Calendar.getInstance();
-        c.setTime(new Date()); // Using today's date
-        c.add(Calendar.DATE, 5); // Adding 5 days
-        String output = sdf.format(c.getTime());
-        form.$("[data-test-id='date'] input").click();
+        $("[data-test-id='date'] input").click();
         String selectAll = Keys.chord(Keys.COMMAND, "a");
-        form.$("[data-test-id='date'] input").sendKeys(selectAll);
-        form.$("[data-test-id='date'] input").sendKeys(output);
-        form.$("[data-test-id='name'] input").setValue("Марина Зинченко-Петрова");
-        form.$("[data-test-id='phone'] input").setValue("+79111440000");
-        form.$("[data-test-id='agreement']").click();
-        form.$(byText("Забронировать")).click();
-        SelenideElement popup = $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-        popup.$(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='date'] input").sendKeys(selectAll);
+        int days = 5; // Adding 5 days
+        String planningDate = generateDate(days);
+        $("[data-test-id='date'] input").sendKeys(planningDate);
+
+        $("[data-test-id='name'] input").setValue("Марина Зинченко-Петрова");
+        $("[data-test-id='phone'] input").setValue("+79111440000");
+        $("[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
+
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
